a8b6b90c037d9d0333c154ad91141a6d74f0544b
G.Jeevitha Priyadarshini <jeeviganapathi@gmail.com>
2024-01-17 09:49:59 -0500

    Added account model

---
+++ src/main/java/Account.java
@@ -0,0 +1,29 @@
+public class Account {
+    private int number;
+    private String name;
+    private long balance;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(long balance) {
+        this.balance = balance;
+    }
+}
